61d5bacb3dc2f9c6be7c70815aa7fedc3a37e094
ParamSurywashi <parmanandchared1997@gmail.com>
2023-02-21 10:11:08 +0530

    updated

--- src/Components/Trending.js
+++ src/Components/Trending.js
@@ -7,6 +7,7 @@ import { ToggleButton, ToggleButtonGroup} from '@mui/material';
 let apiKey = "f7c839883fc085f9357c84ea65a753d0";
 function Trending() {
     const [content, setContent] = useState([]);
+    const [popular, setPopular] = useState([]);
     const [pageClick, setPageClick] = useState(1);
     const [totalPage, setTotalPage] = useState(1);
     const [dayOrWeek, setDayOrWeek] = useState("day");
@@ -20,7 +21,16 @@ function Trending() {
          setTotalPage(response.total_pages);
       })
  }
-  
+ const fetchMovie = ()=>{
+  return fetch(
+  `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&sort_by=popularity.desc`) .then((res)=>res.json())
+  .then((response)=>{
+    console.log(response)
+   setPopular(response.results);
+   setTotalPage(response.total_pages);
+  })
+}
+
       useEffect(() => {
         fetchTrading();
         },[pageClick,dayOrWeek])
@@ -72,6 +82,29 @@ function Trending() {
           (event.target.innerText==="DAY") ? (
           setDayOrWeek("day") ): (setDayOrWeek("week"))
         };
+
+        function FetchPopular(popular, movieType){
+          return (
+               <>
+               <h1>{movieType}</h1>
+         <div id='cardBox'>
+          {console.log(popular)}
+            {popular &&
+                popular.map((card) => (
+                  
+                  <CardBox
+                    key={card.id}
+                    id={card.id}
+                    poster={card.poster_path}
+                    title={card.title || card.name}
+                    date={card.first_air_date || card.release_date}
+                    media_type={card.media_type}
+                    vote_average={Math.floor(card.vote_average * 10)}
+                  />
+                ))}
+          </div>
+          </>
+          )}
       
   return (
     <div>
@@ -90,6 +123,7 @@ function Trending() {
       </div>
       {FetchCard(content,"Movie") }
       {FetchCard(content,"TV Shows") }
+      {FetchPopular(popular,"Most Popular") }
     </div>
   )
 }
